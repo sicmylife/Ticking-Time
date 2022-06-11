b9783496914fbc24752fe5cfb8812c8f6f971cb6
sicmylife <37955758+sicmylife@users.noreply.github.com>
2022-06-11 14:30:48 +0200

    first commit

---
+++ begin.js
@@ -0,0 +1,15 @@
+/*
+  WRITE YOUR SOLUTION HERE
+*/
+
+
+function clock(){
+  const clockNode = document.querySelector('#clock');
+  return setInterval(()=> {
+    let date = new Date();
+    let tick = date.toLocaleTimeString();
+    clockNode.textContent= tick;
+  },1000);
+}
+
+clock();
